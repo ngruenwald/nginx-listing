4335daf2f282b3cf9a19341fd57d9e45a6dec3f4
ngruenwald <40895194+ngruenwald@users.noreply.github.com>
2023-10-18 11:57:32 +0200

    tools: more icon mappings

--- tools/mimimetypes.py
+++ tools/mimimetypes.py
@@ -3,7 +3,12 @@ import logging
 
 from xml.dom import minidom
 
-ext_map = {}
+ext_map = {
+    "config": "text-x-generic",
+    "ini": "text-x-generic",
+    "mib": "text-x-generic",
+    "msi": "application-install",
+}
 
 root = minidom.parse("freedesktop.org.xml.in")
 
@@ -16,7 +21,8 @@ for mime_type in root.getElementsByTagName("mime-type"):
         )
     except Exception:
         logging.warn(f"{type} as no icon")
-        continue
+        # continue
+        icon_name = type.replace("/", "-")
 
     try:
         extensions = [
@@ -24,8 +30,10 @@ for mime_type in root.getElementsByTagName("mime-type"):
             for glob in mime_type.getElementsByTagName("glob")
         ]
         for extension in extensions:
-            ext_map.update({extension: icon_name})
+            ext_map.update({extension.lower(): icon_name})
+            ext_map.update({extension.upper(): icon_name})
     except Exception as error:
         logging.error(f"{type}: {error}")
 
-print(f"ext_map = {json.dumps(ext_map, sort_keys=True, indent=4)};")
+with open("../extensions.js", "w") as stream:
+    stream.writelines(f"ext_map = {json.dumps(ext_map, sort_keys=True, indent=4)};")
